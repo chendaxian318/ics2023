fd9659b3b7b92989748281f795806510e7c4254a
tracer-ics2023 <tracer@njuics.org>
2024-04-17 18:11:37 +0800

    >  compile NEMU 21403000301 ChenXin Linux chenxin-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:11:37 up  7:10,  0 users,  load average: 0.23, 0.18, 0.13

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,8 +24,10 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+  bool*z=NULL;
+  *z=true;
   for(int i=0;i<32;i++)
-  printf("%s\t\t\t %s\t\t\t \t\t\t\n",regs[i],reg_name(i));
+  printf("%s\t\t\t %d\t\t\t \t\t\t\n",regs[i],isa_reg_str2val(regs[i],z));
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
